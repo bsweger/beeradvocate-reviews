a057dc7dcab2a1542bdccfe385acf48b04a2a73a
bsweger <bsweger@gmail.com>
2015-01-06 21:39:18 -0500

    update USER_AGENT

--- beeradvocate/settings.py
+++ beeradvocate/settings.py
@@ -1,7 +1,4 @@
 BOT_NAME = 'beeradvocate'
-
 SPIDER_MODULES = ['beeradvocate.spiders']
 NEWSPIDER_MODULE = 'beeradvocate.spiders'
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
+USER_AGENT = 'BAReview (experimental, single-user crawler)'
